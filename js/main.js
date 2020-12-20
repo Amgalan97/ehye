21e26ee11a947894049ceba56cffa24739b66ca9
Amgalan <Amgalan>
2020-12-20 22:57:09 +0800

    create: trends section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-// var menuButton = document.querySelector(".menu-button");
-// menuButton.addEventListener("click", function () {
-//   document
-//   .querySelector(".navbar-wrapper")
-//   .classList.toggle("navbar-wrapper_mobile");
-// });
-var menuButton = $('.menu-button');
+$(document).ready(function () {
+  var menuButton = $('.menu-button');
   menuButton.on('click', function  () {
     $(".navbar-wrapper").toggleClass('navbar-wrapper_mobile');
     $('.menu-button__line:nth-child(1)').toggleClass('first');
@@ -12,3 +7,15 @@ var menuButton = $('.menu-button');
     $('.menu-button__line:nth-child(3)').toggleClass('last');
     $("body").toggleClass('noscroll');
   });
+
+  var tabsItem = $(".trends-tabs__item");
+  var contentItem = $(".trends__content");
+
+  tabsItem.on("click", function (event) {
+    var activeContent = $(this).attr("data-target");
+    tabsItem.removeClass("trends-tabs__item--active");
+    contentItem.removeClass("trends__content--active");
+    $(activeContent).addClass("trends__content--active");
+    $(this).addClass("trends-tabs__item--active");
+  });
+});
