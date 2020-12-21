08e842fe1389c577cb8a58489519c7520c2b97f3
Amgalan <Amgalan>
2020-12-21 18:42:09 +0800

    crete: reviews section

--- js/main.js
+++ js/main.js
@@ -18,4 +18,29 @@ $(document).ready(function () {
     $(activeContent).addClass("trends__content--active");
     $(this).addClass("trends-tabs__item--active");
   });
+
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+      autoplay: {
+      delay: 7000,
+    },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+      effect: "flip",
+  });
+
+  // Отключение автоплея при наведении
+  $(".swiper-container").hover(function() {
+      (this).swiper.autoplay.stop();
+  }, function() {
+      (this).swiper.autoplay.start();
+  });
 });
